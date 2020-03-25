e370591f1cc55ef0ab1f99c2d646b675486919ef
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-25 02:26:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   
   array = string.split
-  array.each{|n|  n.reverse!}
+  array.collect{|n|  n.reverse!}
   
   answer = array.reduce {|s, word| "#{s}" + " #{word}"}
   answer
